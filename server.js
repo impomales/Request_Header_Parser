4eb22ee6bfbef89c30a6722cba05dc7cf3c85bd8
Isaias <impomales@gmail.com>
2016-08-16 21:36:52 +0000

    basic home page with instructions

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
+app.use(express.static(path.join(__dirname + '/views')))
+
 app.get('/', function(req, res) {
-    res.end('hello...')
+    res.render('index.html')
 })
 
 app.listen(process.env.PORT || 8080, function() {
